eec9637a5353db713209e33509ac127edf3659f4
Daryll Santos <daryll.santos@gmail.com>
2018-04-02 22:35:49 +0800

    Added styling to About page.

--- src/containers/about.js
+++ src/containers/about.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import '.././stylesheets/about.css';
 
 export default class About extends Component {
   render() {
     return(
-      <h1>About</h1>
+      <div className="c row aboutPage">
+        <h1>About the Site</h1>
+        <hr/>
+        <p>"Writing Prompts" is a small app that I created for myself because I was getting tired of looking for these online.</p>
+        <p>This page was created with ReactJS and the create-react-app boilerplate.</p>
+        <p>I plan on creating many more microsites like this.</p>
+      </div>
     );
   }
 }
