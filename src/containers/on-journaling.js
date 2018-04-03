990a2773e47e8963b7d477e59ae0dc09f8fab7b9
Daryll Santos <daryll.santos@gmail.com>
2018-04-03 19:07:38 +0800

    Put something in 'On Journaling' page.

--- src/containers/on-journaling.js
+++ src/containers/on-journaling.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 export default class OnJournaling extends Component {
   render() {
     return(
-      <h1>On Journaling</h1>
+      <div className="c row journalingPage">
+        <h1>On Journaling</h1>
+      </div>
     );
   }
 }
