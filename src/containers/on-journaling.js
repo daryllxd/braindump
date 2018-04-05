a119ff135db47400f75a797d3e82366220325609
Daryll Santos <daryll.santos@gmail.com>
2018-04-05 19:46:45 +0800

    Add journaling page stylesheet.

--- src/containers/on-journaling.js
+++ src/containers/on-journaling.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import '.././stylesheets/on-journaling.css';
 
 export default class OnJournaling extends Component {
   render() {
     return(
       <div className="c journalingPage">
-        <h1>On Journaling</h1>
+        <h1>On Journaling:</h1>
       </div>
     );
   }
